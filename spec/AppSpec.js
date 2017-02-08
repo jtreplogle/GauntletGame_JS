4e33293cbe3b0b74537f6d6bd63d4a1c492d08f9
Jason Replogle <JasonReplogle@MacBook-Pro.local>
2017-02-08 07:51:41 -0600

    Game completion minus minor tweaking

---
+++ spec/AppSpec.js
@@ -0,0 +1,22 @@
+describe("the specs for AppSpec", function () {
+
+  it
+
+});
+
+
+
+
+
+// describe("the specs for enemies", function (){
+
+
+//   it("should have an Gauntlet.Combatants", function() {
+//     expect(Gauntlet.Combatants.Monster).toBeDefined();
+//   })
+
+//   it("should have an Orc", function () {
+//     expect(Gauntlet.Combatants.Orc).toBeDefined();
+//   }
+
+//   });
